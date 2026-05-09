2692ac533ac48d657f9173bd812c4a53ea3f4f14
Ki <yanshekki@gmail.com>
2026-05-09 19:12:00 +0800

    feat(backend): register BybitWebsocketClient

--- apps/backend/src/websocket/websocket.module.ts
+++ apps/backend/src/websocket/websocket.module.ts
@@ -1,10 +1,11 @@
 import { Module, Global } from '@nestjs/common';
 import { BinanceWebsocketClient } from './clients/binance-websocket.client';
+import { BybitWebsocketClient } from './clients/bybit-websocket.client';
 import { WebsocketService } from './websocket.service';
 
 @Global()
 @Module({
-  providers: [BinanceWebsocketClient, WebsocketService],
-  exports: [WebsocketService, BinanceWebsocketClient],
+  providers: [BinanceWebsocketClient, BybitWebsocketClient, WebsocketService],
+  exports: [WebsocketService, BinanceWebsocketClient, BybitWebsocketClient],
 })
 export class WebsocketModule {}
